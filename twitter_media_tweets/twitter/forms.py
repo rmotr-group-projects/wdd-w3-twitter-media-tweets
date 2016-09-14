827b09ba18a9cb9f9d56c561cfb7a69c0e1a9569
Adrian Hintermaier <adrian.hintermaier@gmail.com>
2016-09-14 19:44:26 +0200

    video url validation for the form

--- twitter_media_tweets/twitter/forms.py
+++ twitter_media_tweets/twitter/forms.py
@@ -7,3 +7,12 @@ class TweetForm(forms.Form):
     content = forms.CharField(max_length=140)
     image_url = forms.URLField(required=False)
     video_url = forms.URLField(required=False)
+
+    def clean_video_url(self):
+        data = self.cleaned_data['video_url']
+        if "https://www.youtube.com/embed/" not in data:
+            raise forms.ValidationError("Your video url needs to be in the format \"https://www.youtube.com/embed/id\"")
+
+        # Always return the cleaned data, whether you have changed it or
+        # not.
+        return data
