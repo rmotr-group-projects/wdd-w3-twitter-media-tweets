4035b00d893cef02e83914a60267a261b11df39d
Adrian Hintermaier <adrian.hintermaier@gmail.com>
2016-09-14 19:28:19 +0200

    model adjusted

--- twitter_media_tweets/twitter/models.py
+++ twitter_media_tweets/twitter/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
 
 class User(AbstractUser):
@@ -13,6 +15,15 @@ class Tweet(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True)
     content = models.CharField(max_length=140, blank=True)
     created = models.DateTimeField(auto_now_add=True, null=True)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, null=True)
+    object_id = models.PositiveIntegerField(null=True)
+    media = GenericForeignKey('content_type', 'object_id')
+
+    @property
+    def type(self):
+        if self.media:
+            return self.content_type.model.replace('tweet', '')
+        return "text"
 
 
 class ImageTweet(models.Model):
